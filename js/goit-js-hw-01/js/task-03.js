5790a1d4df5796b8061c9ccedbcc36bd5e90d344
Maks Che <anabolium@gmail.com>
2020-01-19 13:29:13 +0200

    first js lesson, add task-03

---
+++ js/goit-js-hw-01/js/task-03.js
@@ -0,0 +1,14 @@
+const ADMIN_PASSWORD = "password";
+let message;
+
+let userInput = prompt("Авторизация");
+
+if (userInput === null) {
+  message = "Отменено пользователем!";
+} else if (userInput === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else {
+  message = "Доступ запрещен, неверный пароль!";
+}
+
+alert(message);
