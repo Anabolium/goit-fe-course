6006c252684759d1ac9344748d9737e9be48731c
Maks Che <anabolium@gmail.com>
2020-01-19 21:09:20 +0200

    add task-05 and task-06

---
+++ js/goit-js-hw-01/js/task-06.js
@@ -0,0 +1,17 @@
+let input;
+let total = 0;
+
+while (true) {
+  input = prompt("Введите число");
+
+  if (input === null) {
+    break;
+  }
+  
+  input = Number(input);
+
+  total += input;
+}
+
+console.log(total);
+alert(total);
